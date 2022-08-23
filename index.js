3b5fcbf1ccd45b76bc70e14f252f731c98bcf5ca
chris <chrishethertonweb@gmail.com>
2022-08-23 12:01:15 +0200

    first part done

---
+++ index.js
@@ -0,0 +1,14 @@
+function greetMessage(name) {
+
+  console.log(`Good Morning, ${name}`);
+}
+
+greetMessage("John"); // print "Good Morning, John"
+
+
+
+
+// greetUsers(["John", "Peter", "Mark"], greetMessage)
+    // "Good Morning, John"
+    // "Good Morning, Peter"
+    // "Good Morning, Mark"
